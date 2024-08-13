6180eaf71e641e87297b1bc6ea1873ae012836b7
nghiandd-ds <153247416+nghiandd-ds@users.noreply.github.com>
2024-08-13 10:41:10 +0700

    Update basel_GPT.py

--- basel_GPT.py
+++ basel_GPT.py
@@ -70,7 +70,8 @@ def query_message(
     """
     strings, relatednesses = strings_ranked_by_relatedness(query, df)
     introduction = '''
-    Only use the given information below to answer the subsequent question. The given information is secret so you can only provide chapters, articles, and information that related to the questions.
+    Only use the given information below to answer the subsequent question. The given information is secret so you can only provide chapters, articles, 
+    and your understanding of information.
     '''
     
     question = f"\n\nQuestion: {query}"
