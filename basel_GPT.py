dd05970d2a63b67961257c9de847b0a4a984ff31
nghiandd-ds <153247416+nghiandd-ds@users.noreply.github.com>
2024-08-27 16:11:32 +0700

    Update basel_GPT.py

--- basel_GPT.py
+++ basel_GPT.py
@@ -80,8 +80,8 @@ def query_message(
     introduction = '''
     Only use the given information below to answer the subsequent question. The given information is secret so you can only provide chapters, articles, 
     and your understanding of information. If you unable to answer the question based on given facts, just say you don't have the necessary information to answer.
-    If you have to give name of relevent article/chapter, use the following format: 
-    [{Article}](https://www.bis.org/basel_framework/chapter/{The first 3 characters of Chapter article is in}/{The rest of the Chapter article is in}/)
+    If you have to give name of relevent article/chapter, Only return Chapter of Basel Framework: 
+    [{Chapter}](https://www.bis.org/basel_framework/chapter/{The first 3 characters of Chapter}/{The rest of the Chapter}/)
     '''
     
     question = f"\n\nQuestion: {query}"
